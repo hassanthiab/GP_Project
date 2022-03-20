e329c1757c7506515abb46888f9cf6fe02cdac35
unknown <rengonidalee@gmail.com>
2022-03-20 18:48:20 +0200

    tournament tab

--- src/components/Tournament/Tournament.jsx
+++ src/components/Tournament/Tournament.jsx
@@ -1,10 +1,89 @@
 import React from 'react'
-import Sidebar from './Sidebar'
+import { useState } from 'react'
+import dayjs from "dayjs"
+import {data} from "./garbage_data"
+import FilterBar from './Searchbar'
+import Cardview from './Cardview';
+
+const isSameOrAfter = require("dayjs/plugin/isSameOrAfter");
+const isSameOrBefore = require("dayjs/plugin/isSameOrBefore");
+dayjs.extend(isSameOrBefore);
+dayjs.extend(isSameOrAfter);
+
 
 function Tournament() {
+
+  const [allData, setData] = useState(data);
+
+  const generatesizeDataForDropdown = () => {
+    return [...new Set(data.map((item) => item.size))];
+  };
+
+  const handleFilterName = (name) => {
+    const filteredData = data.filter((item) => {
+      const fullName = `${item.name} ${item.coordinator}`;
+      if (fullName.toLowerCase().includes(name.toLowerCase())) {
+        return item;
+      }
+    });
+
+    setData(filteredData);
+  };
+
+  const handleFilterLocation = (location) => {
+    const filteredData = data.filter((item) => {
+      if (item.location.toLowerCase().includes(location.toLowerCase())) {
+        return item;
+      }
+    });
+
+    setData(filteredData);
+  };
+
+  const handleFilterSize = (size) => {
+    const filteredData = data.filter((item) => {
+      if (item.size == size) {
+        return item;
+      }
+    });
+
+    setData(filteredData);
+  };
+
+  const handleFilterDate = (date, field) => {
+    const filteredData = data.filter((item) => {
+      if (field === "from" && dayjs(item.date).isSameOrAfter(dayjs(date))) {
+        return item;
+      }
+    });
+
+    setData(filteredData);
+  };
   return (
-    <Sidebar />
-    
+
+    <div className="container">
+    <div className="row">
+      <div className="col-sm-3">
+      <FilterBar
+            sizes={generatesizeDataForDropdown()}
+            onNameFilter={handleFilterName}
+            onlocationFilter={handleFilterLocation}
+            onsizeFilter={handleFilterSize}
+            onDateFilter={handleFilterDate}
+          />
+      </div>
+      <div className="col-sm-9">
+        <div className="row mt-5">
+                 
+        {allData.map((item) => (
+        <Cardview items={item} key={item.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
   )
 }
 
