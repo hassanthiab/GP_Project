34e747dac30ecd10322c731df11969f81f49fbff
unknown <rengonidalee@gmail.com>
2022-03-30 16:05:10 +0300

    Leaderboard prototype

--- src/components/Homepage/Home.jsx
+++ src/components/Homepage/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Carousel from './Carousel'
 import Cards from './Cards'
 import Footer from './Footer'
+import Filters from '../Leaderboard/Filters'
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
     <React.Fragment>
    <Carousel></Carousel>
     <Cards></Cards>
+    <Filters></Filters>
     <Footer></Footer>
       </React.Fragment>
     </body>
