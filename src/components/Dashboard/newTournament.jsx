160d8aee68a06d79bab950be2565f64526b1f149
mohammadmo116 <mohammad1162002@gmail.com>
2022-04-07 05:52:02 +0300

    a

--- src/components/Dashboard/newTournament.jsx
+++ src/components/Dashboard/newTournament.jsx
@@ -1,20 +1,97 @@
 import styled from 'styled-components'
 import SideBar from './Sidebar'
-import { FormControl, FormLabel, RadioGroup, FormControlLabel,Radio,Checkbox,FormGroup} from '@mui/material'
+import { FormControlLabel,Checkbox,FormGroup} from '@mui/material'
 import FancyInput from '../Login/Input'
 import FancyButton from '../Login/Button' 
 import { blue } from '@mui/material/colors'
 import React,{ useState } from 'react'
-import { Button, Stack} from '@material-ui/core'
+import { Button} from '@material-ui/core'
+import axios from "../axios/axios";
 
 const Input = styled('input')({
     display: 'none',
   });
   
 function NewTournament() {
+  const a = localStorage.getItem("type");
+  const [cat, setCat] = useState([]);
+  const [profilePic, setPic] = useState("");
 
+  const [input, setInput] = useState({
+    name:"",
+    club:"",
+    email:"",
+    size:"",
+    location:"",
+    description:"",
+    date:"",
+    image:"",
+    private:"",
+
+  });
+
+  const [errors, setErrors] = useState({
+    name:"",
+    club:"",
+    email:"",
+    size:"",
+    location:"",
+    description:"",
+    date:"",
+    image:"",
+    private:"",
+
+  });
+
+
+
+  let add=()=>{
+    const formData = new FormData();
+    formData.append('name',input['name']);
+    formData.append('club',input['club']);
+    formData.append('email',input['email']);
+    formData.append('size',input['size']);
+    formData.append('location',input['location']);
+    formData.append('description',input['description']);
+    formData.append('date',input['date']);
+    formData.append('image',profilePic);
+    formData.append('private',input['private']);
+            for (let i = 0; i < cat.length; i++) 
+              formData.append('cat[]', cat[i]);
+
+
+    axios()
+    .post("/api/addTournament",formData)
+    .then((response) => {
+      if(response.status==200){
+        
+      }
+      
+    })
+    .catch((error) => {
+      if(!error.response)
+    return});
+  }
+  let changed = (event, inputId) => {
+    if(inputId=="cat"){
+      let Newcat=[...cat]
+      Newcat.includes(event.target.value)?
+      Newcat=Newcat.filter((item) => item != event.target.value):
+      Newcat.push(event.target.value)
+      setCat(Newcat)
+    
+    }
+    
+  else{
+    let Sinput = { ...input };
+    Sinput[inputId] = event.target.value;
+    setInput(Sinput);
+    console.log(event.target.value)
+  }
+  };
     const [profileimage, setImage] = useState('https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png');
     const imageHandler = (e) => {
+      setPic(e.target.files[0])
         const reader = new FileReader();
         reader.onload = () =>{
           if(reader.readyState === 2){
@@ -42,60 +119,104 @@ function NewTournament() {
     <form className="newUserForm">
       <div className="newUserItem">
         <label>Tournament Name</label>
-        <FancyInput type="text" placeholder="Name" />
+        <FancyInput onChange={(event) => changed(event,"name")} type="text" placeholder="Name" />
+      </div>
+      <div className="newUserItem">
+        <label>Club</label>
+        <FancyInput onChange={(event) => changed(event,"club")} type="text" placeholder="Club" />
       </div>
       <div className="newUserItem">
         <label>Email</label>
-        <FancyInput type="text" placeholder="Email" />
+        <FancyInput onChange={(event) => changed(event,"email")} type="text" placeholder="Email" />
+      </div>
+      <div className="newUserItem">
+        <label>Size</label>
+        <FancyInput onChange={(event) => changed(event,"size")} type="text" placeholder="Size" />
       </div>
       <div className="newUserItem">
         <label>Location</label>
-        <FormControl>
-      <RadioGroup
-        row
-        aria-labelledby="demo-row-radio-buttons-group-label"
-        name="row-radio-buttons-group"
-      >
-        <FormControlLabel value="Nablus" control={<Radio />} label="Nablus" />
-        <FormControlLabel value="Khalil" control={<Radio />} label="Khalil" />
-        <FormControlLabel value="Ramallah" control={<Radio />} label="Ramallah" />
-      </RadioGroup>
-    </FormControl>
+        <FancyInput onChange={(event) => changed(event,"location")} type="text" placeholder="Location" />
       </div>
       <div className="newUserItem">
-        <label>Street</label>
-        <FancyInput type="text" placeholder="Rafidia" />
+        <label>Description</label>
+        <FancyInput onChange={(event) => changed(event,"description")} type="text" placeholder="Description" />
       </div>
+  
+      <div className="newUserItem">
+        <label>Date</label>
+        <FancyInput onChange={(event) => changed(event,"date")} type="text" placeholder="Date" />
+      </div>
+      <div className="newUserItem">
+        <label>Private</label>
+        <FancyInput onChange={(event) => changed(event,"private")} type="text" placeholder="Private" />
+      </div>
+      
       <div className="newUserItem">
         <label>Categoary</label>
         <FormGroup row>
-        <FormControlLabel control={<Checkbox     sx={{
+        <FormControlLabel value={'60'} onChange={(event) => changed(event,"cat")}  control={<Checkbox     sx={{
           color: blue[1200],
           '&.Mui-checked': {
             color: blue[600],
           },
-        }}/>} label="Label" />
-        <FormControlLabel control={<Checkbox      sx={{
+        }}/>} label="60" />
+        <FormControlLabel value={'80'} onChange={(event) => changed(event,"cat")} control={<Checkbox      sx={{
           color: blue[1200],
           '&.Mui-checked': {
             color: blue[600],
           },
-        }}/>} label="Label2" />
-        <FormControlLabel control={<Checkbox      sx={{
+        }}/>} label="80" />
+          <FormControlLabel value={'90'} onChange={(event) => changed(event,"cat")} control={<Checkbox      sx={{
           color: blue[1200],
           '&.Mui-checked': {
             color: blue[600],
           },
-        }}/>} label="Label3" />
+        }}/>} label="90" />
+        <FormControlLabel value={'100'} onChange={(event) => changed(event,"cat")} control={<Checkbox      sx={{
+          color: blue[1200],
+          '&.Mui-checked': {
+            color: blue[600],
+          },
+        }}/>} label="100" />
+          <FormControlLabel value={'110'} onChange={(event) => changed(event,"cat")} control={<Checkbox     sx={{
+          color: blue[1200],
+          '&.Mui-checked': {
+            color: blue[600],
+          },
+        }}/>} label="110" />
+          <FormControlLabel value={'120'} onChange={(event) => changed(event,"cat")} control={<Checkbox     sx={{
+          color: blue[1200],
+          '&.Mui-checked': {
+            color: blue[600],
+          },
+        }}/>} label="120" />
+          <FormControlLabel value={'150'} onChange={(event) => changed(event,"cat")} control={<Checkbox     sx={{
+          color: blue[1200],
+          '&.Mui-checked': {
+            color: blue[600],
+          },
+        }}/>} label="150" />
         </FormGroup>
       </div>
       <div className="newUserItem">
-      <label>Categoary</label>
+      <label>Picture</label>
         <div class="row  "  >
               <div class="col-md-4" >
+              <div
+                className="img-holder"
+                style={{ marginTop: 30, marginLeft: "40%" }}
+              >
+                <img src={profileimage} alt="" id="img" className="img" />
+                </div>
       <label htmlFor="contained-button-file">
 
-        <Input accept="image/*" id="contained-button-file" multiple type="file" />
+      <Input
+                      accept="image/*"
+                      id="contained-button-file"
+                      multiple
+                      type="file"
+                      onChange={imageHandler}
+                    />
         <Button variant="contained" component="span">
           Upload
          </Button>
@@ -113,7 +234,8 @@ function NewTournament() {
       </div>
 
       <div className='col-5'>
-      <FancyButton nameButton="Confirm"></FancyButton>
+      <Button onClick={add} variant="contained" component="span" name="Confirm">Confirm</Button>
+      {/* <FancyButton onClick={add} nameButton="Confirm">dsf</FancyButton> */}
           </div>
 
       <div className='col-4'>
--- src/components/Tournament/Tournament.jsx
+++ src/components/Tournament/Tournament.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { useState, useEffect } from "react";
 import FilterBar from "./Searchbar";
 import Cardview from "./Cardview";
-
+import Pagetop from "../Homepage/Pagetop";
+import SimpleAccordion from "./Sidebar";
 import axios from "../axios/axios";
+
+
+
 
 function Tournament() {
   const [allData, setData] = useState("");
-
   useEffect(() => {
     axios()
       .get("/api/tournaments")
@@ -21,114 +24,6 @@ function Tournament() {
       });
   }, []);
 
-  // const generatesizeDataForDropdown = () => {
-  //   return [...new Set(data.map((item) => item.size))];
-  // };
-
-  // const handleFilterName = (name) => {
-  //   const filteredData = data.filter((item) => {
-  //     const fullName = `${item.name} ${item.coordinator}`;
-  //     if (fullName.toLowerCase().includes(name.toLowerCase())) {
-  //       return item;
-  //     }
-  //   });
-
-import Pagetop from "../Homepage/Pagetop";
-import SimpleAccordion from "./Sidebar";
-
-const isSameOrAfter = require("dayjs/plugin/isSameOrAfter");
-const isSameOrBefore = require("dayjs/plugin/isSameOrBefore");
-dayjs.extend(isSameOrBefore);
-dayjs.extend(isSameOrAfter);
-
-function Tournament() {
-  const [allData, setData] = useState(data);
-  const [filters, setFilter] = useState({
-    size: "",
-    coordinator: "",
-    location: "",
-    before:"",
-    after:"",
-  });
-  const generatesizeDataForDropdown = () => {
-    return [...new Set(data.map((item) => item.size))];
-  };
-
-  const handleFilterName = (name) => {
-    const filteredData = allData.filter((item) => {
-      const fullName = `${item.name} ${item.coordinator}`;
-      if (fullName.toLowerCase().includes(name.toLowerCase())) {
-        return item;
-      }
-    });
-
-
-  //   setData(filteredData);
-  // };
-
-
-  // const handleFilterLocation = (location) => {
-  //   const filteredData = data.filter((item) => {
-  //     if (item.location.toLowerCase().includes(location.toLowerCase())) {
-  //       return item;
-  //     }
-  //   });
-
-  const handleFilterLocation = (location) => {
-    let Sfilter = { ...filters };
-    Sfilter["location"] = location;
-    setFilter(Sfilter);
-    const filteredData = data.filter((item) => {
-      if (item.location.toLowerCase().includes(filters["location"].toLowerCase()) && item.size == filters["size"]) {
-        return item;
-      }
-    });
-
-
-  //   setData(filteredData);
-  // };
-
-
-  // const handleFilterSize = (size) => {
-  //   const filteredData = data.filter((item) => {
-  //     if (item.size == size) {
-  //       return item;
-  //     }
-  //   });
-
-  const handleFilterSize = (size) => {
-    let Sfilter = { ...filters };
-    Sfilter["size"] = size;
-    setFilter(Sfilter);
-    const filteredData = data.filter((item) => {
-      if (filters) {
-        setData
-        return item;
-      }
-    });
-
-
-  //   setData(filteredData);
-  // };
-
-
-  // const handleFilterDate = (date, field) => {
-  //   const filteredData = data.filter((item) => {
-  //     if (field === "from" && dayjs(item.date).isSameOrAfter(dayjs(date))) {
-  //       return item;
-  //     }
-  //   });
-
-  const handleFilterDate = (date, field) => {
-    const filteredData = allData.filter((item) => {
-      if (field === "from" && dayjs(item.date).isSameOrAfter(dayjs(date))) {
-        return item;
-      }
-    });
-
-
-  //   setData(filteredData);
-  // };
   return (
     <React.Fragment>
       <Pagetop></Pagetop>
